b9497de51aa89093617302387af1a9e237c07b5c
unknown <jose360pelico@gmail.com>
2023-08-15 23:57:34 -0600

    first commit adding the project to github

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/register.css">
+    <title>Registrer page</title>
+</head>
+<body>
+    <main>
+        <section class="sectionOne">
+            <div class="contenedorHeader">
+                <div><img src="/image/infinity.svg" alt="imagen"></div>
+                <p>devChallenges</p>
+            </div>
+            <h2>Join thousands of learners from around the world</h2>
+            <h3>Master web development by making real-life projects. There are multiple paths for you to choose</h3>
+        </section>
+        <section class="sectionTwo">
+            <div>
+                <div><img src="/image/envelope-fill.svg" alt="imagen"></div>
+                <input type="text" placeholder="Email" name="email">
+            </div>
+            <div>
+                <div><img src="/image/lock.svg" alt="imagen"></div>
+                <input type="text" placeholder="Password" name="pwd">
+            </div>
+            <button type="submit">Start coding now</button>
+        </section>
+        <section class="sectionThree">
+            <p>or continue with these social profile</p>
+            <p>Adready a member? <a href="login.php">Login</a></p>
+        </section>
+    </main>
+</body>
+</html>
