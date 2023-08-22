e52370e72fb4b7d5c3e12e2b99abf9b30ed88aaf
unknown <jose360pelico@gmail.com>
2023-08-22 13:42:01 -0600

    styles were done

--- views/edit.php
+++ views/edit.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <script src="../script/script.js" defer></script>
     <title>CHANGE INFORMATION</title>
 </head>
+
 <body>
     <main>
         <section class="firstSection">
@@ -15,46 +17,52 @@
                 <p>DevChallenges</p>
             </div>
         </section>
-        <section class="secondSection">
-            <div class="headerForm">
-                <h1>Change info</h1>
-                <p>Changes will be reflected to every services</p>
-            </div>
-            <div>
-                <section class="profileSectionSecond">
-                    <div class="containerPhotoProfile">
-                        <div class="contenedorImagen"><img src="#" alt="-"></div>
-                        <h4 class="fotoedit">CHANGE PHOTO</h4>
-                    </div>
-                    <div class="form">
-                        <form action="POST" id="form" >
-                            <div class="containerInformation">
-                                <label for="nombre">NAME</label>
-                                <input type="text" value="XANTHE NEAL" name="nombre">                    
-                            </div>
-                            <div class="containerInformation">
-                                <label for="Bio">BIO</label>
-                                <input name="Bio" type="text" value="I am a software developer and a big fan of 
-                                    devchallenges...">
-                            </div>
-                            <div class="containerInformation">
-                                <label for="phone">PHONE</label>
-                                <input type="text" value="908249274292" name="phone">
-                            </div>
-                            <div class="containerInformation">
-                                <label for="mail">EMAIL</label>
-                                <input name="mail" type="email" value="xanthe.neal@gmail.com">
-                            </div>
-                            <div class="containerInformation last">
-                                <label for="psw">PASSWORD</label>
-                                <input name="psw" type="text" value="*********">
-                            </div>
-                        </form>
-                    </div>
-                        
-                </section>
-            </div>
-        </section>
+        <div class="contenedorprueba2">
+            <div class="ctparrafo"> <a href="/views/profile.php">&lt; Back</a></div>
+            <section class="secondSection">
+                <div class="headerForm">
+                    <h1>Change info</h1>
+                    <p>Changes will be reflected to every services</p>
+                </div>
+                <div>
+                    <section class="profileSectionSecond">
+                        <div class="containerPhotoProfile">
+                            <div class="contenedorImagen"><img src="#" alt="-"></div>
+                            <h4 class="fotoedit">CHANGE PHOTO</h4>
+                        </div>
+                        <div class="form">
+                            <form action="POST">
+                                <div class="containerInformation">
+                                    <label for="nombre">NAME</label>
+                                    <input type="text" value="XANTHE NEAL" id="nombre" placeholder="Enter your name">
+                                </div>
+                                <div class="containerInformation bio">
+                                    <label for="Bio">BIO</label>
+                                    <input class="biogra" id="Bio" type="text" value="I am a software developer and a big fan of 
+                                    devchallenges..." placeholder="Enter your bio..">
+
+                                </div>
+                                <div class="containerInformation">
+                                    <label for="phone">PHONE</label>
+                                    <input type="text" value="908249274292" id="phone" placeholder="Enter your phone...">
+                                </div>
+                                <div class="containerInformation">
+                                    <label for="mail">EMAIL</label>
+                                    <input id="mail" type="email" value="xanthe.neal@gmail.com" placeholder="Enter your email...">
+                                </div>
+                                <div class="containerInformation last">
+                                    <label for="psw">PASSWORD</label>
+                                    <input id="psw" type="text" value="*********" placeholder="Enter your new password">
+                                </div>
+                                <button class="btnSave">Save</button>
+                            </form>
+                        </div>
+
+                    </section>
+                </div>
+            </section>
+        </div>
+
         <section class="thirdSection">
             <div class="contenedorToogle">
                 <div class="contImg"><img src="#" alt="-"></div>
@@ -71,5 +79,6 @@
             </div>
         </section>
     </main>
-</body> 
-</html> 
+</body>
+
+</html>
